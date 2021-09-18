33af46ea0b06992a98ac1a977089d379361a5455
Lorenzo Hermoso Moreno <lorenzo@MacBook-Pro-de-Lorenzo.local>
2021-09-18 20:17:13 +0200

    working on local, not production

--- pages/api/followers.ts
+++ pages/api/followers.ts
@@ -1,14 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import React from 'react'
 
-import { launchChromium } from 'playwright-aws-lambda'
+import { firefox } from 'playwright'
 
 type Data = {
   name: string | string[]
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const browser = await launchChromium({ headless: true })
+  const browser = await firefox.launch({ headless: true })
   const page = await browser.newPage();
   await page.goto(`https://twitter.com/${req.query.name}`);
   await page.waitForLoadState('networkidle');
